95edfff2e53d922885ee510352216b0ddbd8a469
Roman <lutchenkoroma@gmail.com>
2022-09-01 15:25:14 +0300

    added file with completed  task

---
+++ Diff Two Arrays.js
@@ -0,0 +1,23 @@
+// Diff Two Arrays
+// Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. In other words, return the symmetric difference of the two arrays.
+
+// Note: You can return the array with its elements in any order.
+
+function diffArray(arr1, arr2) {
+    let newArr = [];
+
+    function addItemInArray(a, b) {
+        return a.forEach(item => {
+            b.indexOf(item) === -1 ? newArr.push(item) : null;
+        });
+    }
+
+    addItemInArray(arr1, arr2);
+    addItemInArray(arr2, arr1);
+
+    return newArr;
+}
+
+console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
+console.log(diffArray(["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]));
+console.log(diffArray(["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]));
